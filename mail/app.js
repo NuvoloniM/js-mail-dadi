05950c347efb564b718677830c025e7313454e64
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-15 12:55:29 +0100

    Creazione repo e ciclo for per primo es

---
+++ mail/app.js
@@ -0,0 +1,26 @@
+/*Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+(non usare INCLUDES)*/
+
+// Crea l'array con le email del mio "database"
+
+let dataEmail = ["claranocilla12@gmail.com","simonebest56@gmail.com ", "noemipintaldi@gmail.com ", "nuvoloni.m@gmail.com" ];
+let userEmail = prompt("Inserisci qui la tua email");
+
+let x= 0;
+
+for (let i = 0; i < dataEmail.length; i++) {
+    if (userEmail == dataEmail[i]) {
+        x+=1;
+    } else {
+        x
+    }
+}
+
+if (x==0) {
+    console.log("La tua Email non è nel database")
+} else {
+    console.log("la tua email è presente nel database")
+}
