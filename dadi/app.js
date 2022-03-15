51d7ef50fe1f395536add4c1047123e19263d2de
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-15 13:16:31 +0100

    Comparazione risultati e stampo vincitore in html

--- dadi/app.js
+++ dadi/app.js
@@ -1,5 +1,29 @@
-let userDice = Math.floor((Math.random() * 6) + 1);
-console.log(userDice);
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
 
-let compDice = Math.floor((Math.random() * 6) + 1);
-console.log(compDice);
+
+//genero variabile del lancio del giocatore 
+let userDice = Number(Math.floor((Math.random() * 6) + 1));
+let userResult = document.getElementById('player_result');
+
+//stampo risultato su HTML
+userResult.innerHTML = `il tuo lancio è risultato ${userDice}`;
+
+//genero variabile del lancio del computer
+let compDice = Number(Math.floor((Math.random() * 6) + 1));
+let compResult = document.getElementById('computer_result');
+
+//stampo risultato su HTML
+compResult.innerHTML = `il lancio del computer è risultato ${compDice}`;
+
+//chi vince?
+let result = document.getElementById('result');
+if( userDice > compDice) {
+    result.innerHTML = "Hai vinto!!!";
+} else {
+    result.innerHTML = "Il banco vince. Hai perso!";
+}
