456141af3ef71d267632f0cf1d76f4f60fb8135b
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-15 12:58:48 +0100

    Stampa di risultato ciclo for per email

--- mail/app.js
+++ mail/app.js
@@ -9,6 +9,8 @@ stampa un messaggio appropriato sull’esito del controllo.
 let dataEmail = ["claranocilla12@gmail.com","simonebest56@gmail.com ", "noemipintaldi@gmail.com ", "nuvoloni.m@gmail.com" ];
 let userEmail = prompt("Inserisci qui la tua email");
 
+let prompResult = document.getElementById('prompt_result')
+
 let x= 0;
 
 for (let i = 0; i < dataEmail.length; i++) {
@@ -21,6 +23,8 @@ for (let i = 0; i < dataEmail.length; i++) {
 
 if (x==0) {
     console.log("La tua Email non è nel database")
+    prompResult.innerHTML = 'La tua Email non è nel database'
 } else {
     console.log("la tua email è presente nel database")
+    prompResult.innerHTML = 'la tua email è presente nel database'
 }
